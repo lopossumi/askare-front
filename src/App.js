8510e981596a5fd04812dbb9477a6b8c621ad6ec
Mikko Loponen <mikko.loponen@outotec.com>
2018-02-27 16:49:45 +0200

    semantic UI and some buttons added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Container } from 'semantic-ui-react'
+import { Button, Icon, Link } from 'semantic-ui-react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 const App = () => (
-    <div className="container">
-      hello webpack
-    </div>
-  )
+    <Router>
+    <Container>
+        <h1>Askare</h1>
+        <Button color='green'>Create account</Button>
+        <Button color='blue'>Login</Button>
+        <a href='https://github.com/lopossumi/askareact'><Button color='purple'> {/*Github purple is #6e5494 */}
+        <Icon name='github' /> Source on GitHub
+      </Button></a>
+    </Container>
+    </Router>
+)
   
 export default App
