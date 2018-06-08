b4768f16e0d71712bdf9022723c7c113c63bbccc
Mikko Loponen <mikko.loponen@iki.fi>
2018-06-08 19:24:19 +0300

    fixed validation bug

--- src/services/register.js
+++ src/services/register.js
@@ -6,8 +6,10 @@ const register = async (user) => {
         const response = await axios.post(url, user)
         return response.data
     } catch (error) {
-        console.log(error.response)
-        return {...error.response, error: error.response.statusText}
+        // Either the server responded with a validation error, or cannot connect to server.
+        // Forward the correct message.
+        const errorText = error.response.data.error || error.response.statusText
+        return {...error.response.data, error: errorText }
     }
 }
 
